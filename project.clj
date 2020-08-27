8584d763ba539014d1bd7bde44c660eb8e3c69b2
João Galrito <joao.galrito@hotmail.com>
2020-08-27 01:23:18 +0100

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cli-progress "0.1.0"
+(defproject cli-progress "0.2.0"
   :description "CLI Progress bar with multibar support"
   :url "https://github.com/jgalrito/cli-progress"
   :license {:name "MIT License"
